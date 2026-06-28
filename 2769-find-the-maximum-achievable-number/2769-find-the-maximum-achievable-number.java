1938402117e36d2bcc246935bfc7c93a75350e64
sivamoorthy27 <sivamoorthy27112005@gmail.com>
2026-06-28 22:34:58 +0530

    Time: 1 ms (100%), Space: 43.3 MB (47.64%) - LeetHub

---
+++ 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int theMaximumAchievableX(int num, int t) {
+        return num+(2*t);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
