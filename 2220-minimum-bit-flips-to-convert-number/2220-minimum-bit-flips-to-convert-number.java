db01d3cd3796321aedf128d02dba4d58ba36ffa8
sivamoorthy27 <sivamoorthy27112005@gmail.com>
2026-07-15 22:25:34 +0530

    Time: 0 ms (100%), Space: 42.5 MB (18.6%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        return Integer.bitCount(start ^ goal);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
