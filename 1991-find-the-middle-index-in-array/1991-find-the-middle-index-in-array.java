587a55d07b045c0bb01f847fe607fd8697680fea
SIVAMOORTHY K <sivamoorthy27112005@gmail.com>
2026-07-23 22:44:21 +0530

    Time: 0 ms (100%), Space: 43.5 MB (76.57%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int total = 0;
+        for (int num : nums) {
+            total += num;
+        }
+
+        int leftSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int rightSum = total - leftSum - nums[i];
+
+            if (leftSum == rightSum) {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
